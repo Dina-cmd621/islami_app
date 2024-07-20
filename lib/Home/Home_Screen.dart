af4a7fefcd583f51e480c042f4328cdbdf520f78
Dina Gamal <dinahelal349@gmail.com>
2024-07-21 01:25:03 +0300

    Init project. Added empty Home Screen.

---
+++ lib/Home/Home_Screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'home_screen';
+
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp();
+  }
+}
